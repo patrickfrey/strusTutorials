87ab24b76569485b6b67d01df8ce10a00c7b93df
patrickfrey <patrickpfrey@yahoo.com>
2015-06-26 12:12:22 +0200

    working on fetch data of the MusicBrainz tutorial

---
+++ scripts/step1_fetchData.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Prepare data directory ..."
+mkdir data
+cd data
+echo "Download and uppack the data dump ..."
+wget http://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/20150624-002847/mbdump-cdstubs.tar.bz2
+bzip2 -d mbdump-cdstubs.tar.bz2
+tar -xvf mbdump-cdstubs.tar
+ndocs=`cat mbdump/release_raw | awk -F"\t" '{if(min=="")min=max=$1; if($1>max) {max=$1}; if($1< min) {min=$1}; } END {print int(max/100)}'`
+echo "Create the documents ..."
+mkdir doc
+idoc=0
+while [ $idoc -lt $ndocs ]
+do
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>" > doc/$idoc.xml
+    idoc=`expr $idoc + 1`
+done
+echo "Fill the documents with the content of the dump ..."
+cat mbdump/release_raw\
+  | awk -F"\t" '{FNAME=int($1/100); "<item><id>" $1 "</id><title>" $2 "</title><artist>" $3 "</artist><upc>" $9 "</upc><note>" $10 "</note></item>" >> "doc/"FNAME".xml" }'
+
+cd ..
+echo "Leave the data directory and inspect the result ..."
+echo "Nof Documents: `ls -l data/doc/*.xml | wc -l`"
+echo "Nof entries (example document) `cat data/doc/999.xml | grep item | wc -l`"
+
+
+
+
+
